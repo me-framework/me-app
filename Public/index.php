c9d3586af6aa5e617b941c785c576c0d69f494a3
Me Framework <info@meframework.ir>
2022-02-03 00:54:56 +0330

    Co4 2022-02-03

--- Public/index.php
+++ Public/index.php
@@ -2,6 +2,6 @@
 define('ME_DEBUG', true);
 
 require '../vendor/autoload.php';
-require '../vendor/me-framework/me/src/Me.php';
+require '../vendor/me-framework/me/Me.php';
 $config = include '../Config/web.php';
 (new \me\components\Application($config))->run();
