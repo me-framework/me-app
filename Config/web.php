65c29abe1dbf1a0c223c24d1069ae045006db96a
Me Framework <info@meframework.ir>
2022-02-04 02:31:03 +0330

    Co5 2022-02-04

--- Config/web.php
+++ Config/web.php
@@ -5,12 +5,14 @@ return [
     'language'        => 'fa-IR',
     'timezone'        => 'Asia/Tehran',
     'moduleNamespace' => 'app\Modules',
+    'defaultRoute'    => 'site/default/index',
     'basePath'        => dirname(__DIR__),
     'components'      => [
         'database'     => $database,
         'routeManager' => [
-            'map' => [
-                '' => 'site/default/index',
+            'rules' => [
+                '<module>/<controller>/<id:\d+>' => '<module>/<controller>/view',
+                '<module>/<controller>/<action>' => '<module>/<controller>/<action>',
             ]
         ],
     ],
